adfb94389fad65af36176365aaa778e31ca2c92b
Frank Luo <luoy@miamioh.edu>
2018-11-02 10:25:57 -0400

    fix selinux

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,28 +9,19 @@ class processmaker (
   String $pm_group,
   Stdlib::Absolutepath $pm_server_root,
   String $pm_rpm_name,
-  String $enable_selinux,
 ) {
 
   package { $pm_rpm_name :
     ensure => present,
   }
-
-  file { $pm_server_root:
+  -> file { $pm_server_root:
     ensure  => directory,
     owner   => $pm_user,
     group   => $pm_group,
     recurse => true,
   }
-
   if $facts['os']['family'] == 'RedHat' {
     include ::selinux
-    if is_string($enable_selinux) {
-      $selinux_enabled = str2bool($enable_selinux)
-    } else {
-      $selinux_enabled = $enable_selinux
-    }
-
     if $facts['selinux'] == true {
       selinux::fcontext { 'set selinux processmaker home':
         seltype  => 'httpd_sys_rw_content_t',
